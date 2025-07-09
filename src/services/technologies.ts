940a9e78c0906b02be537e75fa826fc01fbc0267
Brisa Abigaíl Díaz <brisaabigaildiaz2000@gmail.com>
2025-07-09 20:47:47 -0300

    fix: forcing cache update

--- src/services/technologies.ts
+++ src/services/technologies.ts
@@ -3,7 +3,7 @@ const BASE_URL = 'https://api.npoint.io/5b5ac9422e8d33a9e00d'
 
 async function getTechStack(): Promise<TechStack[] | null> {
   try {
-    const res = await fetch(BASE_URL)
+    const res = await fetch(BASE_URL, { cache: 'no-store' })
     const data = await res.json()
     return data.sort((a: TechStack, b: TechStack) => a.id < b.id)
   } catch (error) {
